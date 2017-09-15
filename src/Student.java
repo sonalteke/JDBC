bae6b01648713e8dd3e61777e9bade96d5ec35c7
sonalteke <sonal.teke11@gmail.com>
2017-09-15 12:02:14 +0530

    demo

---
+++ src/Student.java
@@ -0,0 +1,51 @@
+/**
+ * Created by sonal on 15/9/17.
+ */
+public class Student {
+    int id;
+    String name;
+    String city;
+
+    public Student(int id,String name,String city){
+        this.id=id;
+        this.name=name;
+        this.city=city;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+}
---
+++ src/SupplierStudentDemo.java
@@ -0,0 +1,13 @@
+import java.util.function.Supplier;
+
+/**
+ * Created by sonal on 15/9/17.
+ */
+public class SupplierStudentDemo {
+    public static void main(String[] args) {
+        Supplier<Student> supplier=()-> {return new Student(1,"sonal","pune");
+        };
+        Student s=supplier.get();
+        System.out.println("student details:\n" +s.getId()+" "+s.getName()+" "+s.getCity());
+    }
+}
